333dc34f603107aec11ad5016681afa5feb2e549
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-03-29 11:22:52 -0300

    feat: using server.go and env

--- server.go
+++ server.go
@@ -1,11 +1,20 @@
 package main
-import "net/http"
 
-func main(){
-    http.HandleFunc("/", Hello)
-    http.ListenAndServe(":80", nil)
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":5000", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request){
-    w.Write([]byte("<h1> Hello Full Cycle</h1>"))
+func Hello(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("<h1> Hello Full Cycle</h1>"))
+	nome  := os.Getenv("NOME")
+	idade := os.Getenv("IDADE")
+
+	fmt.Fprintf(w, "Hello, Eu sou  %s. Eu tenho %s (ano/anos)", nome, idade)
 }
